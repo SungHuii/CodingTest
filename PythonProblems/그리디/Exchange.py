a96b3774b77d74945ee6ccde81b20408575b3373
홍성휘 <gkemg2017@gmail.com>
2025-09-19 16:43:50 +0900

    그리디: 문제 추가

--- PythonProblems/그리디/Exchange.py
+++ PythonProblems/그리디/Exchange.py
@@ -9,18 +9,65 @@
 # 출력
 # 제출할 출력 파일은 1행으로만 되어 있다. 잔돈에 포함된 매수를 출력하시오.
 
+# 문제 풀이(직접한 내용)
 t = int(input())
+myyen = 1000
 
-a = t // 500
-t = t % 500
-b = t // 100
-t = t % 100
-c = t // 50
-t = t % 50
-d = t // 10
-t = t % 10
-e = t // 5
-t = t % 5
-f = t // 1
+while myyen > 0:
+    a = t // 500
+    myyen = myyen - (a * 500)
+    t %= 500
+    b = t // 100
+    myyen = myyen - (b * 100)
+    t %= 100
+    c = t // 50
+    myyen = myyen - (c * 50)
+    t %= 50
+    d = t // 10
+    myyen = myyen - (d * 10)
+    t %= 10
+    e = t // 5
+    myyen = myyen - (e * 5)
+    t %= 5
+    f = t // 1
+    t %= 1
+    myyen = myyen - (f * 1)
+    result = a + b + c + d + e + f
+    print(result)
+
+# 답안
+price = int(input())
+change = 1000 - price
+
+coins = [500, 100, 50, 10, 5, 1]
+cnt = 0
+for coin in coins:
+    cnt += change // coin
+    change %= coin
+
+print(cnt)
+
+# 답안 2
+t = int(input())        # 물건 가격
+change = 1000 - t       # 거스름돈
+
+a = change // 500
+change -= a * 500
+
+b = change // 100
+change -= b * 100
+
+c = change // 50
+change -= c * 50
+
+d = change // 10
+change -= d * 10
+
+e = change // 5
+change -= e * 5
+
+f = change // 1
+change -= f * 1
+
 result = a + b + c + d + e + f
 print(result)
